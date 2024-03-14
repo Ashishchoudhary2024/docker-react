506e7d83576a9b32bdbe1e230f02de4ba93b4bdc
thegreatashi <thegreatashi@gmail.com>
2024-03-14 16:02:17 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi There, this is ashish, Testing the app
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Docker from Ashish
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
